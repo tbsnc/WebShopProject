e4048194d3c91e8e80181960b986ecc24dd82c12
John Doe <day35ish@outlook.com>
2023-10-15 14:38:52 +0200

    added nuget that applies changes to view while app is running, minor view changes

--- WebShopProject/Program.cs
+++ WebShopProject/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
